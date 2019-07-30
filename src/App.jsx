df5c3cb4b315be81fe827878eedfdfe859329462
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-07-30 12:54:09 -0700

    Create users table

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  ReactDOM.render(<div><h1>Welcome to EBae</h1></div>, document.getElementById('root'));
+});
